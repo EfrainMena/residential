7692e614cff08828d1e4e2eaab952db0afc842b7
josueroly <josuecadenacruz@gmail.com>
2019-04-18 16:46:39 -0400

    migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('username',100)->unique();
             $table->string('name',100);
             $table->string('email',100)->unique();
             $table->string('password',100);
+            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,76 +1,21 @@
-@extends('layouts.sidebar')
-@section('contenido')
-<!-- ============================================================== -->
-<!-- End Bread crumb and right sidebar toggle -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- Container fluid  -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- Bread crumb and right sidebar toggle -->
-<!-- ============================================================== -->
-<div class="page-breadcrumb">
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
     <div class="row">
-        <div class="col-12 d-flex no-block align-items-center">
-            <h4 class="page-title">
-                Inicio
-            </h4>
-            <div class="ml-auto text-right">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li aria-current="page" class="breadcrumb-item active">
-                            Library
-                        </li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- ============================================================== -->
-<!-- End Bread crumb and right sidebar toggle -->
-<!-- ============================================================== -->
-<div class="container-fluid">
-    <!-- ============================================================== -->
-    <!-- Start Page Content -->
-    <!-- ============================================================== -->
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">
-                Basic Datatable
-            </h5>
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered" id="zero_config">
-                    <thead>
-                        <tr>
-                            <th>
-                                Name
-                            </th>
-                            <th>
-                                Position
-                            </th>
-                            <th>
-                                Office
-                            </th>
-                            <th>
-                                Age
-                            </th>
-                            <th>
-                                Start date
-                            </th>
-                            <th>
-                                Salary
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
             </div>
         </div>
     </div>
