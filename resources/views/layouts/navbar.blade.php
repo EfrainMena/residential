b676655c49f5b24fa7eacf98a154ea4002210930
EfrainMena <efra60144273@gmail.com>
2019-04-21 17:33:57 -0400

    arreglos en home(no importante)

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,14 +14,14 @@
                         <b class="logo-icon p-l-10">
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="index/assets/images/logo-icon.png" alt="homepage" class="light-logo" />
+                            <img src="index/assets/images/logo-icon.png" alt="homepage" class="light-logo">
                            
                         </b>
                         <!--End Logo icon -->
                          <!-- Logo text -->
                         <span class="logo-text">
                              <!-- dark Logo text -->
-                             <img src="index/assets/images/logo-text.png" alt="homepage" class="light-logo" />
+                             <h3>{{ Auth::user()->role->name }}</h3>
                             
                         </span>
                         <!-- Logo icon -->
@@ -157,7 +157,7 @@
                         <!-- User profile and search -->
                         <!-- ============================================================== -->
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="index/assets/images/users/d3.jpg" alt="user" class="rounded-circle" width="31">&nbsp;{{ Auth::user()->name }} ({{ Auth::user()->role->name }})</a>
+                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="index/assets/images/users/d3.jpg" alt="user" class="rounded-circle" width="31">&nbsp;{{ Auth::user()->name }}</a>
                             <div class="dropdown-menu dropdown-menu-right user-dd animated">
                                 <a class="dropdown-item" href="javascript:void(0)"><i class="ti-user m-r-5 m-l-5"></i> My Profile</a>
                                 <a class="dropdown-item" href="javascript:void(0)"><i class="ti-wallet m-r-5 m-l-5"></i> My Balance</a>
