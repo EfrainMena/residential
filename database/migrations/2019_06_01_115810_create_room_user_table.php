c1c56054b89f9231afd8e23d9c8742e11ccbcfcc
EfrainMena <efra60144273@gmail.com>
2019-06-01 13:04:18 -0400

    insercion  de tablas room_user, conuntries, states

---
+++ database/migrations/2019_06_01_115810_create_room_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('action', 10);
+            $table->unsignedInteger('room_id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_user');
+    }
+}
